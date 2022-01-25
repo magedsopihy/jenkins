73d23d42333a5fe4345d67475bce064472967a0c
magedsopihy <73051234+magedsopihy@users.noreply.github.com>
2022-01-25 10:40:13 +0200

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -9,10 +9,10 @@ def buildImage(){
     sh 'docker build -t magedsopihy/maven_project:1.0 .'
     sh 'echo $PASS | docker login -u $USERNAME --password-stdin'
     sh 'docker push magedsopihy/maven_project:1.0'
+    }
 }
 
-def deployApp(){
-    echo "deploying the app"
+def deployApp() {
+    echo "deploying the application"
 }
-
 return this
