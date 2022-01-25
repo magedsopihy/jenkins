3d8c7030cbb7933526af4dfb57a75bf408a2cbd1
magedsopihy <73051234+magedsopihy@users.noreply.github.com>
2022-01-25 10:21:09 +0200

    Create script.groovy

---
+++ script.groovy
@@ -0,0 +1,18 @@
+def buildJar(){
+    echo "building the application"
+    sh 'mvn package'
+}
+
+def buildImage(){
+    echo "building the docker image"
+    withCredentials([usernamePassword(credentialsId: 'DOCKER_HUB', passwordVariable: 'PASS', usernameVariable: 'USERNAME')]){
+    sh 'docker build -t magedsopihy/maven_project:1.0 .'
+    sh 'echo $PASS | docker login -u $USERNAME --password-stdin'
+    sh 'docker push magedsopihy/maven_project:1.0'
+}
+
+def deployApp(){
+    echo "deploying the app"
+}
+
+return this
